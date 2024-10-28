0e7e5c580c546666f0d0317b7c4f2b01bde8baf3
AvinashMSangle <avinashMS.3119@proton.me>
2024-10-28 07:33:14 +0000

    UPDATED

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 import "./index.css"
+import { titleCase } from "./utils";
 
-// const showYearEl= document.querySelector(".show-year");
-// showYearEl.textContent = neaw Date().getFullYear();
+const formEl = document.querySelector("[data-form]");
+const inputEl = document.querySelector("[data-user-input]")
+
+const tasks =[];
+
+formEl.addEventListener("submit",(e) =>{
+    e.preventDefault();
+    if(!inputEl.value)return; 
+    
+   
+const newTask ={
+    text:titleCase(inputEl.value),
+    isCompleted: false,
+    id:tasks.length,
+    }
+    tasks.unshift(newTask);
+    
+    console.log(tasks);
+    inputEl.value="";
+   });
+
+const showYearEl = document.querySelector(".show-year");
+showYearEl.textContent = new Date().getFullYear();
